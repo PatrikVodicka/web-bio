d12de25b676deade489394f099117d266e10880b
Patrik Vodička <vodicka.patrik@gmail.com>
2022-09-23 17:18:33 +0200

    Revert "fix wrong 404 on page reload"

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import viteSvgLoader from 'vite-svg-loader'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import pluginRewriteAll from 'vite-plugin-rewrite-all'
 
 const sourcePath = fileURLToPath(new URL('./src', import.meta.url))
 
@@ -11,8 +10,7 @@ export default defineConfig({
   plugins: [
     vue(),
     viteSvgLoader(),
-    vueJsx(),
-    pluginRewriteAll(),
+    vueJsx()
   ],
   resolve: {
     alias: {
